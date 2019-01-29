19ffa28dec9f6df9ef2e5209145eb2a557cb0d8b
Nick Frejneman <nick.frejneman@gmail.com>
2019-01-29 22:55:30 +0100

    Initial commit

---
+++ app/src/main/java/com/example/isavior/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.isavior;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.support.v7.app.ActionBar;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+
+    }
+}
