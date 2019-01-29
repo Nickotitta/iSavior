3c15027fcc565c7a0f9778a4d9068d5cf258d8e4
Nick Frejneman <nick.frejneman@gmail.com>
2019-01-30 00:16:29 +0100

    Aded some new activities

--- app/src/main/java/com/example/isavior/Menu.java
+++ app/src/main/java/com/example/isavior/Menu.java
@@ -1,17 +1,45 @@
 package com.example.isavior;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class Menu extends AppCompatActivity {
+    private ImageButton gamesWithCardsButton;
+    private ImageButton gamesWithTalksButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
 
+
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
+
+        gamesWithCardsButton = (ImageButton)findViewById(R.id.buttonToCardGames);
+        gamesWithCardsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent go = new Intent(Menu.this, gamesWithGames.class);
+                startActivity(go);
+            }
+
+
+        });
+        gamesWithTalksButton = (ImageButton)findViewById(R.id.buttonToTalkGames);
+        gamesWithTalksButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent go = new Intent(Menu.this, gamesWithTalk.class);
+                startActivity(go);
+            }
+
+
+        });
     }
 }
---
+++ app/src/main/java/com/example/isavior/gamesWithGames.java
@@ -0,0 +1,13 @@
+package com.example.isavior;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class gamesWithGames extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_games_with_games);
+    }
+}
